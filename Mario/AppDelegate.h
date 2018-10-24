0ea07479e83a83a93bfa20a33304956b314a737a
Jorge Rangel <jrangel@aidc.mx>
2018-10-24 06:13:12 -0500

    Initial Commit

---
+++ Mario/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mario
+//
+//  Created by Jorge Rangel on 10/24/18.
+//  Copyright © 2018 IDQR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
